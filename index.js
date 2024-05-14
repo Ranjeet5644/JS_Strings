2d40ac683a4a84b23bb0a2ba378591d056727567
Ranjeet Kumar Yadav <138971007+Ranjeet5644@users.noreply.github.com>
2024-05-15 00:57:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log("This is string tutorial")
+
+let name= "Ranjeet"
+
+console.log(name)
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+console.log(name[3])
+console.log(name[4])
+console.log(name[5])
+
+console.log(name.length)
+
+
+let real_name = "ranjeet"
+let friend = "Rohan"
+
+console.log(`His name is ${real_name} and his friend name is ${friend}`)
+
+let b = "Shivam"
+
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+
+console.log(b.slice(1,5))
+console.log(b.slice(1))
+
+console.log(b.replace("S", "R"))
+
+console.log(name.concat(b,"Aaishwariya"))
+
+let c =" Ranjeet "
+let d="Ranjeet"
+
+console.log(c.trim())
+
+console.log(d.charAt(1))
+console.log(d.charAt(11))
+
+
+console.log(d.startsWith("Ra"))
+console.log(d.endsWith("ra"))
